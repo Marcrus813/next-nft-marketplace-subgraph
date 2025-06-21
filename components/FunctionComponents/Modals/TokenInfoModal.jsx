5089843fbd9205e95acf1ecbd26c5bd764720f66
CocoaPuffs <m15388108558_1@163.com>
2025-06-21 07:36:26 +0000

    WIP: Done with `TokenInfoModal`, moving on to buy and update

--- components/FunctionComponents/Modals/TokenInfoModal.jsx
+++ components/FunctionComponents/Modals/TokenInfoModal.jsx
@@ -1,7 +1,9 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Image from "next/image";
 
-import { Dialog, DialogBackdrop, DialogPanel, Transition } from "@headlessui/react";
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, Transition } from "@headlessui/react";
+import { truncateStr } from "@/components/FunctionComponents/Display/NFTBox";
+import { compareAddressIgnoreCase } from "@/utils/web3/addressUtils";
 
 const TokenInfoModal = ({
     isVisible,
@@ -18,7 +20,7 @@ const TokenInfoModal = ({
     formattedPrice,
     connectedAccount,
 }) => {
-    const userIsOwner = seller === connectedAccount;
+    const userIsOwner = compareAddressIgnoreCase(seller, connectedAccount);
 
     const handleBuyBtnClick = () => {
         // TODO: Implement later
@@ -45,23 +47,72 @@ const TokenInfoModal = ({
                     <DialogPanel
                         transition
                         className={
-                            "dialog-panel transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                            "dialog-panel transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                         }
                     >
-                        <div className={"bg-gray-700 px-4 pt-5 pb-4 sm:p-6 sm:pb-4"}>
-                            <div className={"sm:flex sm:items-start"}>
-                                <div
-                                    className={
-                                        "mx-auto flex size-12 shrink-0 items-center justify-center rounded-lg"
-                                    }
-                                >
-                                    <Image
-                                        className={"w-3xs"}
-                                        src={imgUri}
-                                        alt={"NFT"}
-                                        width={"256"}
-                                        height={"256"}
-                                    />
+                        <div className={"mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left"}>
+                            <DialogTitle as={"h2"} className={"text-base text-gray-200"}>
+                                <span>
+                                    <span className={"font-semibold"}>{`${tokenName}(`}</span>
+                                    <span
+                                        className={"italic"}
+                                    >{`${truncateStr(tokenAddress, 15)}`}</span>
+                                    <span className={"font-semibold"}>{`): #${tokenId}`}</span>
+                                </span>
+                            </DialogTitle>
+                        </div>
+                        <div className={"info-container"}>
+                            <div className={"img-container"}>
+                                <Image
+                                    className={"rounded-lg w-full"}
+                                    src={imgUri}
+                                    alt={"NFT"}
+                                    width={"512"}
+                                    height={"512"}
+                                />
+                            </div>
+                            <div className={"details-container"}>
+                                <div className={"details-item"}>
+                                    <span className={"details-title"}>Seller: </span>
+                                    <span className={"details-content"}>{seller}</span>
+                                </div>
+                                <div className={"details-item"}>
+                                    <span className={"details-title"}>Token address: </span>
+                                    <span className={"details-content"}>{tokenAddress}</span>
+                                </div>
+                                <div className={"details-item"}>
+                                    <span className={"details-title"}>Token ID: </span>
+                                    <span className={"details-content"}>{tokenId}</span>
+                                </div>
+                                <div className={"details-item"}>
+                                    <span className={"details-title"}>Price: </span>
+                                    <span className={"details-content"}>{formattedPrice}</span>
+                                </div>
+                                <div className={"btn-container"}>
+                                    <button
+                                        className={"close-btn"}
+                                        type={"button"}
+                                        onClick={onClose}
+                                    >
+                                        Close
+                                    </button>
+                                    {userIsOwner ? (
+                                        <button
+                                            className={"update-btn"}
+                                            type={"button"}
+                                            onClick={handleUpdateBtnClick}
+                                        >
+                                            Update Listing
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className={"buy-btn"}
+                                            type={"button"}
+                                            onClick={handleBuyBtnClick}
+                                        >
+                                            Buy NFT
+                                        </button>
+                                    )}
                                 </div>
                             </div>
                         </div>
